8d0ebe57e9ae7a1e524455f20a33cafabef810de
rajkonkret <rajkonkret660@gmail.com>
2024-03-19 09:28:05 +0100

    2 - update typy danych krotka - add sorted()

--- typy_danych3-krotka.py
+++ typy_danych3-krotka.py
@@ -13,3 +13,35 @@ print(type(tupla4))  # <class 'tuple'>
 temp = 36, 6
 print(temp)  # (36, 6)
 print(type(temp))  # <class 'tuple'>
+
+tuple_names = "Radek", "Tomek", "Zenek", "Bartek"
+tuple_names2 = ("Radek", "Tomek", "Zenek", "Bartek")
+print(type(tuple_names))  # <class 'tuple'>
+print(type(tuple_names2))  # <class 'tuple'>
+
+# są immutable
+# temp[0] = 1  # TypeError: 'tuple' object does not support item assignment
+# del temp[0] - usunięcie elementu z krotki SyntaxError: cannot delete expression
+del temp  # usunięcie całej krotki(tupli) z pamięci
+# print(temp)  # NameError: name 'temp' is not defined
+
+print(tuple_names[0])
+print(tuple_names[1:3])
+print(tuple_names[:3])
+print(tuple_names[2:])
+# Radek
+# ('Tomek', 'Zenek')
+# ('Radek', 'Tomek', 'Zenek')
+# ('Zenek', 'Bartek')
+print(tuple_names[-1])  # ostatni element Bartek
+print(tuple_names[:])  # wypisanie całej tupli ('Radek', 'Tomek', 'Zenek', 'Bartek')
+
+# sprawdzenie czy dany element istnieje w tupli
+print("Radek" in tuple_names)  # True
+
+print(tuple_names.count("Tomek"))  # 1 policzenie ile razy elemnt występuje
+print(tuple_names.index("Tomek"))  # index 1 - indeks elementu
+
+# sortowanie sorted()
+print(sorted(tuple_names))  # sortowanie zwróci listę ['Bartek', 'Radek', 'Tomek', 'Zenek']
+print(sorted(tuple_names, reverse=True))  # ['Zenek', 'Tomek', 'Radek', 'Bartek']
