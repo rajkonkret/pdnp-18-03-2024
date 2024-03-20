003f4e5cf8d65289831d6da93adaf9d14f879761
rajkonkret <rajkonkret660@gmail.com>
2024-03-20 10:31:57 +0100

    3 - add csv zad1

---
+++ csv_zad1.py
@@ -0,0 +1,36 @@
+# csv - dane oddzielone znakiem podziału ,;tab i podobne
+import csv
+
+# biblioteka do pracy z pilikami csv
+
+fields = ['name', 'branch', 'year', 'cgpa']
+row = ['radek', 'coe', '3', '0.1']
+
+dictionary = dict(zip(fields, row))
+print(dictionary)  # {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': '0.1'}
+
+dict_list = [
+    {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': '0.1'},
+    {'name': 'marek', 'branch': 'cos', 'year': '2', 'cgpa': '8.1'},
+    {'name': 'tomek', 'branch': 'cot', 'year': '1', 'cgpa': '6'},
+    {'name': 'zenek', 'branch': 'coy', 'year': '7', 'cgpa': '67.4'},
+]
+filename = 'records.csv'
+
+with open(filename, 'w') as csv_f:
+    # zapis wiersz po wierszu
+    csvwriter = csv.writer(csv_f)
+    csvwriter.writerow(fields)
+    csvwriter.writerow(row)
+
+filename = 'records2.csv'
+with open(filename, 'w', newline='') as f:
+    # zapisanie do csv plików ze słownika
+    csvwriter = csv.DictWriter(f, fieldnames=fields, delimiter=";")
+    csvwriter.writeheader()  # zapisanie nagłówków
+    # zapisanie pojedynczego słownika (eden wiersz)
+    # csvwriter.writerow(dictionary)
+    # zapisanie wielu wierszy z listy słowników
+    csvwriter.writerows(dict_list)
+# newline='' - ominięcie pustych linii
+# delimiter= - znak podziału
